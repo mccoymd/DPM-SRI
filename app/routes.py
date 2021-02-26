854d0624a2f821a5aaeb731fc028b80ffb987069
mccoymd <mdm299@sri-appdev.c.mccoylab.internal>
2021-02-26 22:25:30 +0000

    landing page update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Dynamic Precision Medicine - SRI"
+    analysisOptions = {'simulate': 'Simulate a New Patient Population',
+                       'analyze': 'Analyze an Existing Simulation',
+                       'experiment': 'Analyze Experimental Data'} 
+    return render_template('index.html',
+                           title='Home',
+                           analysisOptions=analysisOptions)
+
---
+++ launchAPP.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+export FLASK_APP=runSRI.py
+export FLASK_ENV=development
+
+python3 -m flask run --host=0.0.0.0 --port=5000
