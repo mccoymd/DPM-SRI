0afa658dc0b1f94fc6c17f5c41ff9e41c7f2377a
mccoymd <mdm299@sri-appdev.c.mccoylab.internal>
2021-03-02 23:49:07 +0000

    update a buncha stuff

--- README.txt
+++ README.txt
@@ -3,3 +3,8 @@ export FLASK_APP=runSRI.py
 export FLASK_ENV=development
 ####flask run 
 python3 -m flask run --host=0.0.0.0 --port=5000
+
+
+to run on the linux machine, you can simply run:
+
+bash launchApp.sh
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,20 @@
 from flask import render_template
 from app import app
+from app.forms import Parameter2012Form
 
 @app.route('/')
 @app.route('/index')
 def index():
     analysisOptions = {'simulate': 'Simulate a New Patient Population',
                        'analyze': 'Analyze an Existing Simulation',
-                       'experiment': 'Analyze Experimental Data'} 
+                       'experiment': 'Review Experimental Data'} 
     return render_template('index.html',
-                           title='Home',
+                           title='DPM-SRI',
                            analysisOptions=analysisOptions)
 
+
+@app.route('/analyze')
+def analyze():
+    title = 'Analyze an Existing Simulation'
+    form = Parameter2012Form()
+    return render_template('analyze.html', title=title, form=form)
