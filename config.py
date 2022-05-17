da8e6382822b47e7e3668bcb6a89fa6d50b34f32
Cutler Simpson <cutler.simpson@gmail.com>
2022-05-17 11:02:42 -0700

    Initialize postgres db with 4 tables

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql:///' + os.path.join(basedir, 'dpm_dev')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
