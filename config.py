5d22c4d2e7c49a9aea7e00b5198672c657cae0cc
Cutler Simpson <cutler.simpson@gmail.com>
2022-05-17 11:46:29 -0700

    Add remaining fields to db tables

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql:///' + os.path.join(basedir, 'dpm_dev')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql:///dpm_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
